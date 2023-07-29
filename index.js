7266b14d2fcd66f3773598fd51a6065df3966eb2
Isai <delosreyesbeats@gmail.com>
2023-07-28 21:51:45 -0700

    Final

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Defining Circle, Square and Triangle modules
+// Defining SVG class
+
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shapes");
